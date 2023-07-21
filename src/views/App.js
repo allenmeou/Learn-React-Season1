b1db24cf007f4864dc694d6dd3cfee0b583cfc44
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-07-21 11:17:48 +0700

    uploap code #3.3

--- src/views/App.js
+++ src/views/App.js
@@ -1,9 +1,11 @@
 import "../styles/global.scss";
+import MyComponents from "./Example/MyComponents";
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <MyComponents />
     </div>
   );
 }
---
+++ src/views/Example/MyComponents.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class MyComponents extends React.Component {
+  render() {
+    return <h1>My Components</h1>;
+  }
+}
+
+export default MyComponents;
