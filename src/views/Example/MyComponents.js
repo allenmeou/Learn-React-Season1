93543092e007ef39c1ef03f048ae428ac0867796
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-07-21 11:58:10 +0700

    uploap code #5

--- src/views/Example/MyComponents.js
+++ src/views/Example/MyComponents.js
@@ -1,8 +1,34 @@
 import React from "react";
 
 class MyComponents extends React.Component {
+  state = {
+    name: "meou",
+    chanel: "meou chanel",
+  };
+
+  handleChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
   render() {
-    return <h1>My Components</h1>;
+    return (
+      <div>
+        <div>
+          <p>My Components</p>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={(event) => this.handleChangeName(event)}
+          />
+          <h1> My name is : {this.state.name}</h1>
+        </div>
+        <div>
+          <h2>My Youtube Chanel : {this.state.chanel}</h2>
+        </div>
+      </div>
+    );
   }
 }
 
