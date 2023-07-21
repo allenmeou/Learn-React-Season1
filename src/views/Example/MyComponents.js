bb13ef6b4aec0195cb1c090d343f37a95d01514a
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-07-21 16:19:02 +0700

    uploap code #8

--- src/views/Example/MyComponents.js
+++ src/views/Example/MyComponents.js
@@ -3,9 +3,12 @@ import React from "react";
 class MyComponents extends React.Component {
   // STATE
   state = {
-    name: "meou",
-    chanel: "meou chanel",
+    name: "Minamo",
+    chanel: "Minamo Jav chanel",
+    title: "",
+    image: "",
   };
+
   // EVENT
   handleChangeName = (event) => {
     this.setState({
@@ -13,25 +16,34 @@ class MyComponents extends React.Component {
     });
   };
   handleClickButton = () => {
-    console.log("clicked");
-    alert("Đã Đăng Ký");
+    this.setState({
+      chanel: "JAV Chanel",
+      title: "Cảm ơn vì đăng ký , tặng bạn tấm hình để ngắm nhé ^^",
+      image: "https://scanlover.com/assets/images/11110-UKZH0An1YAonmvWl.jpeg",
+    });
   };
+
   // RENDER
   render() {
+    console.log(">>> call render :", this.state);
     return (
       <div>
         <div>
-          <p>My Components</p>
+          <h1>My Favorite JAV</h1>
           <input
             type="text"
             value={this.state.name}
             onChange={(event) => this.handleChangeName(event)}
           />
-          <h1> My name is : {this.state.name}</h1>
+          <h2> My name is : {this.state.name}</h2>
         </div>
         <div>
-          <h2>My Youtube Chanel : {this.state.chanel}</h2>
+          <h3>My Youtube Chanel : {this.state.chanel}</h3>
           <button onClick={() => this.handleClickButton()}>Đăng ký</button>
+        </div>
+        <div>
+          <h4>{this.state.title}</h4>
+          <img src={this.state.image} alt="" />
         </div>
       </div>
     );
